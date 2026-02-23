7857fe19dc7cb8bfa7eb785829ca25e9f4f5a1e1
yvillanova <yurimoreiravillanova1@gmail.com>
2026-02-23 17:47:22 -0300

    feat(aula-02): implement ktor server boilerplate with health check

--- server/src/main/kotlin/com/fatec/merge_skills_kmp/Application.kt
+++ server/src/main/kotlin/com/fatec/merge_skills_kmp/Application.kt
@@ -1,20 +1,47 @@
 package com.fatec.merge_skills_kmp
 
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
 fun main() {
     embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
+@Serializable
+data class HealthResponse(
+    val status: String,
+    val timestamp: Long
+)
+
 fun Application.module() {
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+            ignoreUnknownKeys = true
+        })
+    }
+
     routing {
         get("/") {
-            call.respondText("Ktor: ${Greeting().greet()}")
+            call.respondText("API LDDM/PDM (mergeskillskmp). Tente acessar /api/health")
+        }
+
+        get("/api/health") {
+            call.respond(
+                HealthResponse(
+                    status = "Status UP",
+                    timestamp = System.currentTimeMillis()
+                )
+            )
         }
     }
 }
