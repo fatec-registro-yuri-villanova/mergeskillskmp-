777adffb52f9cb50663aeda3bb408401af545ad4
yvillanova <yurimoreiravillanova1@gmail.com>
2026-02-23 17:45:05 -0300

    feat(aula-01): add ktor-client and serialization to shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    
+    alias(libs.plugins.kotlinSerialization)
 }
 
 kotlin {
@@ -9,7 +9,11 @@ kotlin {
     
     sourceSets {
         commonMain.dependencies {
-            // put your Multiplatform dependencies here
+            api(libs.ktor.client.core)
+            implementation(libs.ktor.client.cio)
+            api(libs.ktor.client.content.negotiation)
+            api(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.kotlinx.serialization.json)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
