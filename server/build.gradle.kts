eeb1cbc8a3241182465a77a8e3d5d61544aa270e
yvillanova <yurimoreiravillanova1@gmail.com>
2026-03-02 18:07:15 -0300

    fix(desktop+server): add missing ktor content negotiation in server and resolve desktop syntax fix

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -18,6 +18,8 @@ dependencies {
     implementation(libs.logback)
     implementation(libs.ktor.serverCore)
     implementation(libs.ktor.serverNetty)
+    implementation(libs.ktor.serverContentNegotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.supabase.postgrest)
     implementation(libs.supabase.gotrue)
     implementation(libs.supabase.storage)
