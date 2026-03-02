a1e316122878ac0bc7484b0fb003c82d125f339f
yvillanova <yurimoreiravillanova1@gmail.com>
2026-03-02 18:52:02 -0300

    fix(desktop+server+client): sync client dependencies with aula-03 and remove ktor plugin from server

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -9,11 +9,15 @@ kotlin {
     
     sourceSets {
         commonMain.dependencies {
+            implementation(libs.kotlinx.serialization.json)
+            api(libs.kotlinx.datetime)
             api(libs.ktor.client.core)
             implementation(libs.ktor.client.cio)
             api(libs.ktor.client.content.negotiation)
             api(libs.ktor.serialization.kotlinx.json)
-            implementation(libs.kotlinx.serialization.json)
+            api(libs.supabase.postgrest)
+            api(libs.supabase.gotrue)
+            api(libs.supabase.storage)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
